1be7ae14d9668edf6ce6e3a18f0d00afa730fbe1
Arnaldo Torres <torres.arnaldo817@gmail.com>
2020-09-01 22:03:15 -0400

    add dependencies gitignore

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
